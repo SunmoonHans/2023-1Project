9b853397ef2625c5770eee9c4e93142873f2f9da
SunmoonHans <73623725+SunmoonHans@users.noreply.github.com>
2023-04-03 10:13:13 +0900

    Create Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y, hap = 0;
+    
+    cout << "두 수를 입력받아서 더하는 문제를 완성하시오. : " ;
+    
+    cin >> x >> y;
+    
+    hap = x + y;
+    
+    cout << "두 수의 합은 " << hap << "입니다." << endl;
+    
+    return 0;
+}
