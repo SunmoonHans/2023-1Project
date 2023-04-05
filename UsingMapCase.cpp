998582e2ce54014e075e35998ba4bba74eee5aa4
SunmoonHans <73623725+SunmoonHans@users.noreply.github.com>
2023-04-05 09:24:03 +0900

    Create UsingMapCase.cpp

---
+++ UsingMapCase.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+#define MAX 101
+using namespace std;
+
+string S;
+map<char, int> m;
+
+int main(void)
+{
+    cin >> S;
+
+    cout << "문자열을 입력하시오 : "; 
+
+    for (int i = 97; i < 123; i++)
+    {
+        m.insert(make_pair(i, -1));
+    }
+
+    for (int i = 0; i < S.length(); i++)
+    {
+        for (auto it = m.begin(); it != m.end(); it++)
+        {
+            if (S[i] == it->first && it->second == -1) it->second = i;
+        }
+    }
+
+    for (auto it = m.begin(); it != m.end(); ++it) cout << it->second << endl;
+
+    system("pause");
+    return 0;
+}
