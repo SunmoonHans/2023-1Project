b4adaadef687ba6d55dd0f2bcdcf4983d792af93
SunmoonHans <73623725+SunmoonHans@users.noreply.github.com>
2023-04-11 10:35:52 +0900

    Add files via upload

---
+++ ConsoleApplication1809.cpp
@@ -0,0 +1,19 @@
+﻿// ConsoleApplication1809.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "(___)" << endl;
+    cout << "(o o)____ /" << endl;
+    cout << " @@      \\" << endl;
+    cout << " \\ ____, /" << endl;
+    cout << " //   //" << endl;
+    cout << " ^^  ^^" << endl;
+
+    system("pause");
+    return 0;
+}
+
+
